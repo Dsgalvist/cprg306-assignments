07e2ff281ba813cfc6149b0f14b281d2bce62e27
Dsgalvist <Diegogalvis682@gmail.com>
2026-01-20 21:22:42 -0700

    Fix StudentInfo to use Next.js Link

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function Page() {
+  return (
+    <div>
+      <p>CPRG 306: Web Development 2 - Assignments</p>
+      <Link href="/week-2">Go to Week 2</Link>
+    </div>
+  )
+}
---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Name: Diego Galvis Tapasco</p>
+            <p> GitHub: <Link href="https://github.com/Dsgalvist"> https://github.com/Dsgalvist</Link>
+            </p>
+        </div>
+    );
+}
